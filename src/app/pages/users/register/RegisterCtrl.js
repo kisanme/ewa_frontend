23370ac4b3cf5c7c4cfb8b67839aa7c1065535ea
Nasik <nasik2ms@gmail.com>
2017-11-16 09:08:55 +0530

    Registration scaffolding

--- src/app/pages/users/register/RegisterCtrl.js
+++ src/app/pages/users/register/RegisterCtrl.js
@@ -10,90 +10,35 @@
 
   /** @ngInject */
   function ProfilePageCtrl($scope, $rootScope, fileReader, $filter, $uibModal, User) {
-    $scope.picture = $filter('profilePicture')('Nasta');
 
     // Hide sidebar and header when these pages are loaded
     $rootScope.sidebar_hide = true;
     $rootScope.header_hide = true;
 
-    $scope.removePicture = function () {
-      $scope.picture = $filter('appImage')('theme/no-photo.png');
-      $scope.noPicture = true;
-    };
-
-    $scope.uploadPicture = function () {
-      var fileInput = document.getElementById('uploadFile');
-      fileInput.click();
-
-    };
-
-    $scope.socialProfiles = [
-      {
-        name: 'Facebook',
-        href: 'https://www.facebook.com/akveo/',
-        icon: 'socicon-facebook'
-      },
-      {
-        name: 'Twitter',
-        href: 'https://twitter.com/akveo_inc',
-        icon: 'socicon-twitter'
-      },
-      {
-        name: 'Google',
-        icon: 'socicon-google'
-      },
-      {
-        name: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/akveo',
-        icon: 'socicon-linkedin'
-      },
-      {
-        name: 'GitHub',
-        href: 'https://github.com/akveo',
-        icon: 'socicon-github'
-      },
-      {
-        name: 'StackOverflow',
-        icon: 'socicon-stackoverflow'
-      },
-      {
-        name: 'Dribbble',
-        icon: 'socicon-dribble'
-      },
-      {
-        name: 'Behance',
-        icon: 'socicon-behace'
-      }
-    ];
-
-    $scope.unconnect = function (item) {
-      item.href = undefined;
-    };
-
-    $scope.showModal = function (item) {
-      $uibModal.open({
-        animation: false,
-        controller: 'ProfileModalCtrl',
-        templateUrl: 'app/pages/profile/profileModal.html'
-      }).result.then(function (link) {
-          item.href = link;
-        });
-    };
-
-    $scope.getFile = function () {
-      fileReader.readAsDataUrl($scope.file, $scope)
-          .then(function (result) {
-            $scope.picture = result;
-          });
-    };
-
-    $scope.switches = [true, true, false, true, true, false];
-
-      // When the app navigates away from this controller, re-load the header and sidebar in
+    // When the app navigates away from this controller, re-load the header and sidebar in
     $scope.$on('$stateChangeStart', function (event) {
       $rootScope.header_hide = false;
       $rootScope.sidebar_hide = false;
     });
+
+    $scope.validatePassword = function () {
+      if ($scope.password === $scope.confirm_password) {
+        return true;
+      }
+      return false;
+    };
+
+    $scope.signUp = function () {
+      if ($scope.validatePassword() == true) {
+        var sign_up_obj = {
+          email: $scope.email,
+          number: $scope.number,
+          password: $scope.password
+        }
+      } else {
+        console.log("Password mismatching");
+      }
+    };
   }
 
 })();
